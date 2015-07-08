a893ab283043a40bc90e28e925a95d77c3318ce8
Cedric Staub <cs@squareup.com>
2015-07-08 11:51:07 -0700

    Support graceful in-place upgrades via SIGUSR1

--- server/server.go
+++ server/server.go
@@ -6,13 +6,20 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"os/signal"
 	"runtime"
+	"sync"
+	"syscall"
 
+	"github.com/kardianos/osext"
 	"github.com/kavu/go_reuseport"
 )
 
@@ -20,6 +27,10 @@ var listenAddress = "127.0.0.1:8043"
 var privateKeyPath = "server.key"
 var certChainPath = "server.crt"
 var caBundlePath = "ca-bundle.crt"
+
+func init() {
+	log.SetPrefix(fmt.Sprintf("[%5d] ", os.Getpid()))
+}
 
 func panicOnError(err error) {
 	if err != nil {
@@ -60,6 +71,10 @@ func parsePrivateKey(data []byte) (key crypto.PrivateKey, err error) {
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var gracefulChild bool
+	flag.BoolVar(&gracefulChild, "graceful", false, "send sigterm to parent after startup")
+	flag.Parse()
 
 	caBundleBytes, err := ioutil.ReadFile(caBundlePath)
 	panicOnError(err)
@@ -104,20 +119,102 @@ func main() {
 
 	log.Printf("Listening on %s", listenAddress)
 
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	stopper := make(chan bool, 1)
+
+	go accept(listener, wg, stopper)
+	go sigtermHandler(listener, stopper)
+	go sigusr1Handler()
+
+	if gracefulChild {
+		parent := syscall.Getppid()
+		log.Printf("Sending SIGTERM to parent PID %d", parent)
+		syscall.Kill(parent, syscall.SIGTERM)
+	}
+
+	log.Printf("Startup completed, waiting for connections")
+
+	wg.Wait()
+
+	log.Printf("All connections closed, shutting down")
+}
+
+func sigtermHandler(listener net.Listener, stopper chan bool) {
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGTERM)
+
+	<-signals
+	stopper <- true
+
+	log.Printf("Got SIGTERM, closing listening socket")
+	signal.Stop(signals)
+	listener.Close()
+}
+
+func sigusr1Handler() {
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGUSR1)
+
+	for {
+		<-signals
+
+		log.Printf("Received SIGUSR1, attempting restart")
+		go reexec()
+	}
+}
+
+func reexec() {
+	path, err := osext.Executable()
+	if err != nil {
+		log.Printf("Failed to get executable path: %s", err)
+	}
+
+	log.Printf("Executing self: %s", path)
+
+	cmd := exec.Command(path, "-graceful")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+
+	if err != nil {
+		log.Printf("Child failed with error: %s", err)
+	}
+}
+
+func accept(listener net.Listener, wg *sync.WaitGroup, stopper chan bool) {
+	defer wg.Done()
+	defer listener.Close()
+
 	for {
 		conn, err := listener.Accept()
+
+		// Check if we're supposed to stop
+		select {
+		case _ = <-stopper:
+			return
+		default:
+		}
+
 		if err != nil {
 			log.Printf("Error accepting connection: %s", err)
 			continue
 		}
-		go handle(conn)
+
+		wg.Add(1)
+		go handle(conn, wg)
 	}
+
+	log.Printf("Closing listening socket")
 }
 
-func handle(conn net.Conn) {
+func handle(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer conn.Close()
+
 	log.Printf("New connection from %s", conn.RemoteAddr())
 
-	defer conn.Close()
 	n, err := io.Copy(os.Stdout, conn)
 
 	if err == nil {
