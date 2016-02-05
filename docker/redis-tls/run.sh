ddd46b1f7774f7612bea9912a1b6df35e47d7257
Cedric Staub <cs@squareup.com>
2016-02-05 10:22:04 -0800

    Drop privileges inside container

--- docker/redis-tls/run.sh
+++ docker/redis-tls/run.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+REDIS_USER=redis
 REDIS_SOCKET=/tmp/redis.sock
 
 # Launch ghostunnel
 # Terminate redis if tunnel shuts down
 (
-  /go/bin/ghostunnel --listen 0.0.0.0:6379 --target unix:$REDIS_SOCKET "$@"
+  sudo -u $REDIS_USER /go/bin/ghostunnel --listen 0.0.0.0:6379 --target unix:$REDIS_SOCKET "$@"
   redis-cli -s $REDIS_SOCKET shutdown
 ) &
 
 # Launch redis
-redis-server /etc/redis/redis.conf
+sudo -u $REDIS_USER redis-server /etc/redis/redis.conf
