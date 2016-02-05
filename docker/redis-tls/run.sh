3c2201e7701b259385fa5a5d33d74acac403a5df
Cedric Staub <cs@squareup.com>
2016-02-05 11:36:55 -0800

    Use USER to drop privs in container

--- docker/redis-tls/run.sh
+++ docker/redis-tls/run.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-REDIS_USER=redis
 REDIS_SOCKET=/tmp/redis.sock
+
+# Launch redis
+redis-server /etc/redis/redis.conf &
+
+while ! [ -S $REDIS_SOCKET ]; do
+  echo "Waiting for $REDIS_SOCKET to appear..."
+  sleep 1
+done
 
 # Launch ghostunnel
 # Terminate redis if tunnel shuts down
 (
-  sudo -u $REDIS_USER /go/bin/ghostunnel --listen 0.0.0.0:6379 --target unix:$REDIS_SOCKET "$@"
+  /go/bin/ghostunnel --listen 0.0.0.0:6379 --target unix:$REDIS_SOCKET "$@"
   redis-cli -s $REDIS_SOCKET shutdown
 ) &
 
-# Launch redis
-sudo -u $REDIS_USER redis-server /etc/redis/redis.conf
+# Wait for redis; terminate tunnel if redis stops
+wait %1
+killall ghostunnel
