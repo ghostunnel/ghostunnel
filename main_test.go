dddc74cc15577824f9c08e8b8d7874040cffd66d
Cedric Staub <cs@css.bio>
2016-03-05 10:19:07 -0800

    Add more tests for main

--- main_test.go
+++ main_test.go
@@ -18,9 +18,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -30,6 +32,14 @@ func TestIntegrationMain(t *testing.T) {
 	// We're wrapping it in a test case so that we can record the test coverage.
 	isIntegration := os.Getenv("GHOSTUNNEL_INTEGRATION_TEST")
 
+	// Catch panics to make sure test exits normally and writes coverage
+	// even if we got a crash (we might want to test error cases)
+	defer func() {
+		if err := recover(); err != nil {
+			t.Error(err)
+		}
+	}()
+
 	if isIntegration == "true" {
 		var wrappedArgs []string
 		err := json.Unmarshal([]byte(os.Getenv("GHOSTUNNEL_INTEGRATION_ARGS")), &wrappedArgs)
@@ -37,6 +47,22 @@ func TestIntegrationMain(t *testing.T) {
 
 		run(wrappedArgs)
 	}
+}
+
+func TestInitLoggerSyslog(t *testing.T) {
+	*useSyslog = true
+	initLogger()
+	assert.NotNil(t, logger, "logger should never be nil after init")
+}
+
+func TestPanicOnError(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Error("panicOnError should panic, but did not")
+		}
+	}()
+
+	panicOnError(errors.New("error"))
 }
 
 func TestFlagValidation(t *testing.T) {
@@ -108,4 +134,48 @@ func TestDisallowsFooDotCom(t *testing.T) {
 	assert.False(t, validateUnixOrLocalhost("alocalhost.com:1234"), "alocalhost.com should be disallowed")
 	assert.False(t, validateUnixOrLocalhost("localhost.com.foo.com:1234"), "localhost.com.foo.com should be disallowed")
 	assert.False(t, validateUnixOrLocalhost("74.122.190.83:1234"), "random ip address should be disallowed")
+}
+
+func TestServerBackendDialerError(t *testing.T) {
+	*serverForwardAddress = "invalid"
+	_, err := serverBackendDialer()
+	assert.NotNil(t, err, "invalid forward address should not have dialer")
+
+	*serverForwardAddress = "unix:does-not-exist"
+	_, err = serverBackendDialer()
+	assert.NotNil(t, err, "invalid forward address should not have dialer")
+}
+
+func TestClientBackendDialerError(t *testing.T) {
+	*keystorePath = ""
+	_, err := clientBackendDialer(nil)
+	assert.NotNil(t, err, "cannot create client dialer with invalid keystore")
+}
+
+func waitWithTimeout(started chan bool) bool {
+	select {
+	case up := <-started:
+		return up
+	case <-time.After(10 * time.Second):
+		panic("timed out waiting for channel")
+	}
+}
+
+func TestClientListenError(t *testing.T) {
+	started := make(chan bool, 1)
+	*keystorePath = ""
+	*statusAddr, _ = net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	clientListen(started, nil, nil)
+
+	if waitWithTimeout(started) {
+		t.Error("client listener should not start with invalid params")
+	}
+
+	*statusAddr = nil
+	*clientListenAddress = "invalid"
+	clientListen(started, nil, nil)
+
+	if waitWithTimeout(started) {
+		t.Error("client listener should not start with invalid params")
+	}
 }
