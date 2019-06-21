ce148f1c3ea7537453c82a611960aa5f84108af4
Cedric Staub <cs@squareup.com>
2019-06-21 10:20:55 -0700

    Fix documentation for socket package

---
+++ socket/doc.go
@@ -0,0 +1,4 @@
+// Package socket provides method for parsing addresses and opening
+// listening sockets, supporting direct TCP/UNIX sockets and sockets
+// provided by launchd/systemd for socket activation.
+package socket
