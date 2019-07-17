cb449105356b950e8ef8c7739c3c07a7a8c7bfe1
Markus Blaschke <mblaschke82@gmail.com>
2019-07-17 22:25:41 +0200

    Add cipher suite AES-UNSAFE

--- tls.go
+++ tls.go
@@ -25,6 +25,9 @@ import (
 )
 
 var cipherSuites = map[string][]uint16{
+	"AES-UNSAFE": {
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+	},
 	"AES": {
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
