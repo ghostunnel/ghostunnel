05ec6380fb469aa8f68a2520179018c3a10ffd1d
Marcos Yacob <marcos.yacob@hpe.com>
2022-07-04 12:13:55 -0300

    update dependency
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- docs/spiffe-workload-api-demo/03-run-frontend-agent.sh
+++ docs/spiffe-workload-api-demo/03-run-frontend-agent.sh
@@ -4,9 +4,9 @@ set -e
 
 # Refresh the bootstrap bundle
 spire-server bundle show \
-    -registrationUDSPath ./spire/server.sock > ./spire/bootstrap.crt
+    -socketPath ./spire/server.sock > ./spire/bootstrap.crt
 
 # Run the frontend agent
 spire-agent run \
         -config ./spire/frontend-agent.conf \
-        -joinToken "$(spire-server token generate -registrationUDSPath ./spire/server.sock -spiffeID spiffe://domain.test/frontend-agent | awk '{print $2}')"
+        -joinToken "$(spire-server token generate -socketPath ./spire/server.sock -spiffeID spiffe://domain.test/frontend-agent | awk '{print $2}')"
