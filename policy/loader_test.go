4e2678caaeb679c4b46efe4d7a6a50592c7df18a
Cedric Staub <css@css.bio>
2022-11-03 18:48:10 -0700

    Add some additional unit tests

--- policy/loader_test.go
+++ policy/loader_test.go
@@ -41,10 +41,43 @@ import input
 default allow := true
 `
 
+func TestPolicyInitFail(t *testing.T) {
+	p, err := LoadFromFile("invalid", "invalid")
+	assert.Nil(t, p, "policy should be nil")
+	assert.NotNil(t, err, "error should not be nil")
+}
+
+func TestPolicyReloadFail(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// Skip on Windows due to temp file handling issues
+		t.Skip()
+	}
+
+	f, err := ioutil.TempFile("", "policy")
+	assert.Nil(t, err, "temp file error")
+	defer os.Remove(f.Name())
+
+	_, err = f.WriteAt([]byte(allowFoobarPolicy), 0)
+	_ = f.Sync()
+	assert.Nil(t, err, "temp file write error")
+
+	p, err := LoadFromFile(f.Name(), "data.policy.allow")
+	assert.NotNil(t, p, "policy was unexpectedly nil")
+	assert.Nil(t, err, "error loading policy")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Remove + reload to test failure
+	os.Remove(f.Name())
+	err = p.Reload()
+	assert.NotNil(t, err, "error should not be nil")
+}
+
 func TestPolicyReloading(t *testing.T) {
 	if runtime.GOOS == "windows" {
-		// TODO: Fix temp file handling on Windows
-		return
+		// Skip on Windows due to temp file handling issues
+		t.Skip()
 	}
 
 	f, err := ioutil.TempFile("", "policy")
